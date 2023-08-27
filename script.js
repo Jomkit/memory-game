10dfc44b5ae5d407d5d8fcaafe846b77e23b6907
Jomkit <j.jujaroen96@gmail.com>
2023-08-28 00:33:37 +0900

    cards now change color, and js checks for matches. Still need to get non-matches to revert to no bg color

--- script.js
+++ script.js
@@ -42,12 +42,13 @@ let shuffledColors = shuffle(COLORS);
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
+
   for (let color of colorArray) {
     // create a new div
     const newDiv = document.createElement("div");
 
     // give it a class attribute for the value we are looping over and add .card and a random color
-    newDiv.classList.add("card", color);
+    newDiv.classList.add( color, "card");
 
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
@@ -55,13 +56,70 @@ function createDivsForColors(colorArray) {
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
+
 }
 
 // TODO: Implement this function!
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  //console.log("you just clicked", event.target);
+  const color = event.target.classList[0];
+  event.target.style.setProperty("background-color", color);
+  
 }
+
+function clickCounter(){
+  let clickCnt = 0;
+  let colorCheck = [];
+  let cards = [];
+  
+  gameContainer.addEventListener('click', function(e){
+    //console.log("You just clicked", e.target);
+    if(e.target.classList[1] === "card"){
+      //console.log("You clicked a card");
+      clickCnt ++;
+      console.log(clickCnt);
+
+      colorCheck.push(e.target.classList[0]);
+      console.log("The colors are:", colorCheck);
+    };
+
+    //if statements that check for match
+    if(clickCnt == 2){
+      if(colorCheck[0] === colorCheck[1]){
+        console.log("Match!");
+        clickCnt = 0;
+        colorCheck = [];
+      }else {
+        console.log("No Match!");
+        clickCnt = 0;
+        console.log("Removing Colors");
+        setTimeout(() => {
+          console.log("removed");
+          const color1 = document.getElementsByClassName(colorCheck[0])[0];
+          const color2 = document.getElementsByClassName(colorCheck[1])[0];
+
+          color1.style.removeProperty("background-color");
+          color2.style.removeProperty("background-color");
+          colorCheck = [];
+        }, 1000);
+      };
+    };
+
+  });
+}
+
+
+// function resetCards(array){
+//   console.log("removed");
+//   const color1 = document.getElementsByClassName(array[0])[0];
+//   const color2 = document.getElementsByClassName(array[1])[0];
+
+//   color1.style.removeProperty("background-color");
+//   color2.style.removeProperty("background-color");
+
+// }
 
 // when the DOM loads
 createDivsForColors(shuffledColors);
+clickCounter();
